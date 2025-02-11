165c19b0f2af3b3afd79dbfecdf8c65ec3d3710d
Christopher Frost <frostyjumper25@gmail.com>
2025-02-10 22:24:19 -0600

    finish-index.js

--- Develop/Develop/index.js
+++ Develop/Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 import fs from 'fs';
 import inquirer from 'inquirer';
+import {generateMarkdown} from './utils/generateMarkdown.js'
 
 // TODO: Create an array of questions for user input
 const questions = [ {
@@ -51,11 +52,29 @@ const questions = [ {
   },
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('README.md file has been generated!');
+        }
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+      const markdownContent = generateMarkdown(answers); 
+      writeToFile('README.md', markdownContent);
+    })
+    .catch((error) => {
+      console.error('Error gathering user input:', error);
+    });
+}
 
 // Function call to initialize app
 init();
